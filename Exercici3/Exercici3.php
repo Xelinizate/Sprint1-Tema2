2d17a17af475f08a7e8ebb98e5e1f900f00a95aa
Adriana CP <xelinizate86@gmail.com>
2024-09-26 11:29:14 +0200

    Extraer HTML

--- Exercici3/Exercici3.php
+++ Exercici3/Exercici3.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
     <?php
         $X = 3;
         $Y = 6;
@@ -41,5 +34,3 @@
 
 
     ?>
-</body>
-</html>
--- Exercicio6/Exercicio6.php
+++ Exercicio6/Exercicio6.php
@@ -1,24 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
 
-        function isBitten(){
-            return rand(0,1) === 1;
-        }
+<?php
 
-        if (isBitten()):
-            echo "Charlie em va mossegar el dit!";
-        else:
-            echo "Charlie no em va mossegar el dit!";
-        endif;
-    
+function mossegar(){
+    if (rand()&1):
+        echo "Charlie em va mossegar el dit!";
+    else:
+        echo "Charlie no em va mossegar el dit!";
+    endif;
+}
 
-    ?>
-</body>
-</html>
+
+?>
