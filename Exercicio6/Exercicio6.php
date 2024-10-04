05d7815931ee3cd0b17803e2c74d0b5d2ce64b89
Adriana CP <xelinizate86@gmail.com>
2024-10-04 12:17:06 +0200

    Optimitzacio codi

--- Exercicio6/Exercicio6.php
+++ Exercicio6/Exercicio6.php
@@ -2,8 +2,7 @@
 <?php
 
         function isBitten(){
-            $charlie;
-            $resposta;
+    
             
             $charlie = (rand()&1) ? "Charlie em va mossegar el dit!" : "Charlie no em va mossegar el dit!";
             $resposta = ($charlie) ? TRUE : FALSE;
